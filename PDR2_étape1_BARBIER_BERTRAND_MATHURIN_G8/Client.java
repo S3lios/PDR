4cbeec3e88f85a8cdbbcf9c7f57cdbd2b6e8b8f2
Thelium <tom.bertrand23@gmail.com>
2023-02-24 08:14:29 +0100

    ajout rapport et demo

---
+++ PDR2_étape1_BARBIER_BERTRAND_MATHURIN_G8/Client.java
@@ -0,0 +1,161 @@
+import java.rmi.*;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ServiceConfigurationError;
+import java.net.*;
+
+public class Client extends UnicastRemoteObject implements Client_itf {
+
+	private static Map<Integer, SharedObject> objetsPartages;
+	private static Client client;
+
+	public Client() throws RemoteException {
+		super();
+	}
+
+
+///////////////////////////////////////////////////
+//         Interface to be used by applications
+///////////////////////////////////////////////////
+
+	// initialization of the client layer
+	public static void init() {
+		Client.objetsPartages = new HashMap<Integer, SharedObject>();
+		try {
+			client = new Client();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// lookup in the name server
+	public static SharedObject lookup(String name) {
+		try {
+			Server_itf serv = (Server_itf) Naming.lookup(Server.URL);
+			int id = serv.lookup(name);
+
+			SharedObject so = objetsPartages.get(id);
+			if (id != -1 && so == null) {
+				so = new SharedObject(null, id);
+				objetsPartages.put(id, so);
+			}
+			return so;
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	
+	// binding in the name server
+	public static void register(String name, SharedObject_itf so) {
+		SharedObject s = (SharedObject) so;
+		try {
+			Server_itf serv = (Server_itf) Naming.lookup(Server.URL);
+			serv.register(name, s.id);
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			e.printStackTrace();
+			throw new ServiceConfigurationError("Echec de l'enregistrement de l'objet");
+		}
+		
+	}
+
+	// creation of a shared object
+	public static SharedObject create(Object o) {
+		try {
+			Server_itf serv = (Server_itf) Naming.lookup(Server.URL);
+			int id = serv.create(o);
+			SharedObject so = new SharedObject(o, id);
+			objetsPartages.put(id, so);
+			return so;
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			e.printStackTrace();
+			throw new ServiceConfigurationError("Echec de la création de l'objet");
+		}
+	}
+
+	public void callBack(int id) throws java.rmi.RemoteException {
+		SharedObject so = objetsPartages.get(id);
+		so.callBack();
+	}
+	
+/////////////////////////////////////////////////////////////
+//    Interface to be used by the consistency protocol
+////////////////////////////////////////////////////////////
+
+	// subscribe for callback
+	public static void subscribe(int id) {
+		try {
+			Server_itf serv = (Server_itf) Naming.lookup(Server.URL);
+			serv.subscribe(id, client);
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			e.printStackTrace();
+			throw new ServiceConfigurationError("Echec lock_read Client");
+		}
+	}
+
+	// unsuscribe for callback
+	public static void unsubscribe(int id) {
+		try {
+			Server_itf serv = (Server_itf) Naming.lookup(Server.URL);
+			serv.unsubscribe(id, client);
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			e.printStackTrace();
+			throw new ServiceConfigurationError("Echec lock_read Client");
+		}
+	}
+
+
+	// request a read lock from the server
+	public static Object lock_read(int id) {
+		try {
+			Server_itf serv = (Server_itf) Naming.lookup(Server.URL);
+			return serv.lock_read(id, client);
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			e.printStackTrace();
+			throw new ServiceConfigurationError("Echec lock_read Client");
+		}
+	}
+
+	// request a write lock from the server
+	public static Object lock_write (int id) {
+		try {
+			Server_itf serv = (Server_itf) Naming.lookup(Server.URL);	
+			return serv.lock_write(id, client);
+		} catch (MalformedURLException | RemoteException | NotBoundException e) {
+			e.printStackTrace();
+			throw new ServiceConfigurationError("Echec lock_write Client");
+		}
+	}
+
+	public static void unlock(int id) {
+		try {
+			Server_itf serv = (Server_itf) Naming.lookup(Server.URL);
+			serv.unlock(id, client);
+		} catch (RemoteException | MalformedURLException | NotBoundException e) {
+			e.printStackTrace();
+			throw new ServiceConfigurationError("Echec lock_write Client");
+		}
+	}
+
+	// receive a lock reduction request from the server
+	public Object reduce_lock(int id) throws java.rmi.RemoteException {
+		SharedObject so = objetsPartages.get(id);
+		return so.reduce_lock();
+	}
+
+
+	// receive a reader invalidation request from the server
+	public void invalidate_reader(int id) throws java.rmi.RemoteException {
+		SharedObject so = objetsPartages.get(id);
+		so.invalidate_reader();	
+	}
+
+
+	// receive a writer invalidation request from the server
+	public Object invalidate_writer(int id) throws java.rmi.RemoteException {
+		SharedObject so = objetsPartages.get(id);
+		return so.invalidate_writer();
+	}
+}
---
+++ PDR2_étape1_BARBIER_BERTRAND_MATHURIN_G8/Server.java
@@ -0,0 +1,88 @@
+//import java.net.InetAddress;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class Server extends UnicastRemoteObject implements Server_itf{
+
+    public static final String URL = "//localhost:4080/Server";
+    public static final Integer port = 4080;
+    private Map<String, Integer> identifiantsObj;
+    private Map<Integer, ServerObject> servObj;
+
+    public Server() throws RemoteException{
+        super();
+        identifiantsObj = new HashMap<String, Integer>();
+        servObj = new HashMap<Integer, ServerObject>();
+    }
+
+    @Override
+    public int lookup(String name) throws RemoteException {
+        if (identifiantsObj.get(name) == null) {
+            return -1;
+        } else {
+            return identifiantsObj.get(name);
+        }
+    }
+
+    @Override
+    public void register(String name, int id) throws RemoteException {
+        identifiantsObj.put(name, id);
+    }
+
+    @Override
+    public int create(Object o) throws RemoteException {
+        int id = servObj.size();
+        ServerObject sobj = new ServerObject(o,id);
+        servObj.put(id, sobj);
+        return id;
+    }
+
+    @Override
+    public Object lock_read(int id, Client_itf client) throws RemoteException {
+		ServerObject sro = servObj.get(id);
+		return sro.lock_read(client);
+    }
+
+    @Override
+    public Object lock_write(int id, Client_itf client) throws RemoteException {
+        ServerObject sro = servObj.get(id);
+        return sro.lock_write(client);
+    }
+
+    @Override
+    public void unlock(int id, Client_itf client) throws RemoteException {
+       ServerObject sro = servObj.get(id);
+       sro.unlock(client);
+    }
+
+    @Override
+    public void subscribe(int id, Client_itf client) throws RemoteException {
+        ServerObject sro = servObj.get(id);
+        sro.subscribe(client);
+    }
+
+    @Override
+    public void unsubscribe(int id, Client_itf client) throws RemoteException {
+        ServerObject sro = servObj.get(id);
+        sro.unsubscribe(client);
+    }
+    
+    public static void main(String[] args) {
+        try {
+            Registry registry = LocateRegistry.createRegistry(port);
+            Server serveur = new Server();;
+            Naming.rebind(Server.URL, serveur);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    
+}
